9e45f9498b8b2c91268a732497bd8cdaa09ca27e
rafael1789 <rafael89valencia@gmail.com>
2015-05-17 20:19:02 -0600

    Add the ability to subscribe to channels.

--- middlewares/chat.rb
+++ middlewares/chat.rb
@@ -12,13 +12,21 @@ module ChatDemo
     def initialize(app)
       @app     = app
       @clients = []
+      @base_channel = "websockets"
       uri      = URI.parse(ENV["REDISCLOUD_URL"])
       @redis   = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+
       Thread.new do
         redis_sub = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-        redis_sub.subscribe(CHANNEL) do |on|
-          on.message do |channel, msg|
-            @clients.each {|ws| ws.send(msg) }
+        redis_sub.psubscribe("#{@base_channel}.*") do |on|
+          on.pmessage do |pattern, channel, msg|
+            @clients.each do |client| 
+              channel_name = channel.gsub("#{@base_channel}.", "")
+              if client[:channels].include?(channel_name)
+                client[:ws].send(msg)
+              end
+            end
+
           end
         end
       end
@@ -27,22 +35,44 @@ module ChatDemo
     def call(env)
     	if Faye::WebSocket.websocket?(env)
     		ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
+        
+        request = Rack::Request.new(env)
 
-    		ws.on :open do |event|
-			  p [:open, ws.object_id]
-			  @clients << ws
-			end
+        client = { :ws => nil, :channels => [] }
+        
+        # The list of channels the client is requesting access to
+        channels = request.params["channels"]
 
-			ws.on :message do |event|
-			  p [:message, event.data]
-			  @redis.publish(CHANNEL, sanitize(event.data))
-			end
+      	ws.on :open do |event|
+          # Assign the websocket object to the client
+          client[:ws] = ws
 
-			ws.on :close do |event|
-			  p [:close, ws.object_id, event.code, event.reason]
-			  @clients.delete(ws)
-			  ws = nil
-			end
+          # For every channel the client wants to subscribe to...
+          channels.each do |channel|
+            # Ensure they are authorized to listen on this channel. (This is not
+            # needed, but useful if you want to add security to specific channels)
+            #if WebsocketChannelAuthorizer.can_subscribe?(channel, token)
+              # Add the channel to the client
+              client[:channels].push(channel)
+            #end
+          end
+
+          # Add the client to the list of clients
+  			  p [:open, ws.object_id]
+  			  @clients << client
+  			end
+
+  			ws.on :message do |event|
+          json = JSON.parse(event.data)
+  			  p [:message, event.data]
+  			  @redis.publish("#{@base_channel}.#{json['channel']}", sanitize(event.data))
+  			end
+
+  			ws.on :close do |event|
+  			  p [:close, ws.object_id, event.code, event.reason]
+  			  @clients.delete(ws)
+  			  ws = nil
+  			end
 			            
         # Return async Rack response
     		ws.rack_response
